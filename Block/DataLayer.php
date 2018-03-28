4cf75f1f46c4ef0a802abeeac900dcf63df969a5
Renon Stewart <s.renon@gmail.com>
2018-03-28 17:51:51 -0400

    Update copyright

--- Block/DataLayer.php
+++ Block/DataLayer.php
@@ -5,6 +5,7 @@
  *
  * Copyright © MagePal LLC. All rights reserved.
  * See COPYING.txt for license details.
+ * http://www.magepal.com | support@magepal.com
  */
 
 namespace MagePal\GoogleTagManager\Block;
