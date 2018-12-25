a577a6ca8219b6e6c7f06d599daa5ee6294de2ba
Renon Stewart <s.renon@gmail.com>
2018-12-24 19:33:40 -0500

    Support for Magento 2.3 and PHP 7.2

--- view/frontend/web/js/datalayer.js
+++ view/frontend/web/js/datalayer.js
@@ -10,32 +10,34 @@ define([
     'Magento_Customer/js/customer-data',
     'jquery',
     'underscore'
-], function(customerData, $, _){
+], function (customerData, $, _) {
     'use strict';
 
     var lastPushedCart = {};
     var lastPushedCustomer = {};
 
-    function objectKeyExist(object){
-        return _.some(object, function(o) {
+    function objectKeyExist(object)
+    {
+        return _.some(object, function (o) {
             return !_.isEmpty(_.pick(o, ["customer", "cart"]));
         })
     }
 
-    function updateDataLayer(_gtmDataLayer, _dataObject, _forceUpdate){
+    function updateDataLayer(_gtmDataLayer, _dataObject, _forceUpdate)
+    {
         var customer = {isLoggedIn : false},
             cart = {hasItems : false};
 
-        if(_gtmDataLayer !== undefined && (!objectKeyExist(_gtmDataLayer) || _forceUpdate)){
-            if(_.isObject(_dataObject) && _.has(_dataObject, 'customer')){
+        if (_gtmDataLayer !== undefined && (!objectKeyExist(_gtmDataLayer) || _forceUpdate)) {
+            if (_.isObject(_dataObject) && _.has(_dataObject, 'customer')) {
                 customer = _dataObject.customer;
             }
 
-            if(_.isObject(_dataObject) &&_.has(_dataObject, 'cart')){
+            if (_.isObject(_dataObject) && _.has(_dataObject, 'cart')) {
                 cart = _dataObject.cart;
             }
 
-            if(!_.isEqual(lastPushedCart, cart) || !_.isEqual(lastPushedCustomer, customer)){
+            if (!_.isEqual(lastPushedCart, cart) || !_.isEqual(lastPushedCustomer, customer)) {
                 _gtmDataLayer.push({"event": 'mpCustomerSession', "customer" : customer, "cart" : cart});
 
                 lastPushedCustomer = customer;
@@ -55,7 +57,7 @@ define([
             updateDataLayer(gtmDataLayer, _dataObject, true);
         }, this);
 
-        if(!_.contains(customerData.getExpiredKeys(), "magepal-gtm-jsdatalayer")){
+        if (!_.contains(customerData.getExpiredKeys(), "magepal-gtm-jsdatalayer")) {
             updateDataLayer(gtmDataLayer, dataObject(), false);
         }
 
